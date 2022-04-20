6406c65ac230d0efcab079266b15997f709db2f5
developer <no294933me@mail.com>
2022-04-20 19:53:10 +0500

    Format with `prettier`

--- src/views/Cemetery/Cemetery.js
+++ src/views/Cemetery/Cemetery.js
@@ -6,12 +6,9 @@ import Bank from '../Bank';
 import { Alert } from '@material-ui/lab';
 import { Box, Container, Typography, Grid } from '@material-ui/core';
 
-
 import UnlockWallet from '../../components/UnlockWallet';
 import Page from '../../components/Page';
 import CemeteryCard from './CemeteryCard';
-import GenesisCard from './GenesisCard';
-import GenLPCard from './GenLPCard';
 import CemeteryImage from '../../assets/img/bg.png';
 import { createGlobalStyle } from 'styled-components';
 
@@ -36,63 +33,31 @@ const Cemetery = () => {
           <BackgroundImage />
           {!!account ? (
             <Container maxWidth="lg">
-              <Typography color="primary.dark" align="center" variant="h2" style={{ marginTop: "-30px" }}>
+              <Typography color="primary.dark" align="center" variant="h2" style={{ marginTop: '-30px' }}>
                 Farms
               </Typography>
 
               <Box mt={5}>
-                {/* <div hidden={activeBanks.filter((bank) => bank.sectionInUI === 2).length === 0}>
-                  <Typography color="primary.dark" align='center' variant="h4" gutterBottom style={{ marginBottom: '3%' }}>
-                    Earn HSHARE by staking LP
-                  </Typography>
-                  <Grid container spacing={3}>
-                    <CemeteryCard />
-                  </Grid>
-                </div> */}
                 <div hidden={activeBanks.filter((bank) => bank.sectionInUI === 2).length === 0}>
-                  <Typography color="primary.dark" align='center' variant="h4" gutterBottom style={{ marginTop: "-25px", marginBottom: "35px" }}>
+                  <Typography
+                    color="primary.dark"
+                    align="center"
+                    variant="h4"
+                    gutterBottom
+                    style={{ marginTop: '-25px', marginBottom: '35px' }}
+                  >
                     SNOSHARE Pools
                   </Typography>
-                  <Alert variant="filled" severity="info" style={{ marginTop: "-25px", marginBottom: "35px" }}>
-                    All below pools have ended. Please unstake and stake at <a style={{ color: "#fff" }} href="https://arctic-fox.finance/farms">Arctic Fox.</a>
+                  <Alert variant="filled" severity="info" style={{ marginTop: '-25px', marginBottom: '35px' }}>
+                    All below pools have ended. Please unstake and stake at{' '}
+                    <a style={{ color: '#fff' }} href="https://arctic-fox.finance/farms">
+                      Arctic Fox.
+                    </a>
                   </Alert>
                   <Grid container spacing={3}>
                     <CemeteryCard />
                   </Grid>
                 </div>
-{/* 
-                <div hidden={activeBanks.filter((bank) => bank.sectionInUI === 1).length === 0}>
-                  <Typography color="textPrimary" variant="h4" gutterBottom style={{ marginTop: '20px' }}>
-                    Earn TOMB by staking LP
-                  </Typography>
-                  <Alert variant="filled" severity="warning">
-                    All below pools have ended. Please unstake and collect your rewards.
-                  </Alert>
-                  <Grid container spacing={3} style={{ marginTop: '20px' }}>
-                    {activeBanks
-                      .filter((bank) => bank.sectionInUI === 1)
-                      .map((bank) => (
-                        <React.Fragment key={bank.name}>
-                          <CemeteryCard bank={bank} />
-                        </React.Fragment>
-                      ))}
-                  </Grid>
-                      </div> 
-
-                <div hidden={activeBanks.filter((bank) => bank.sectionInUI === 0).length === 0}>
-                  <Typography color="textPrimary" variant="h4" gutterBottom style={{ marginTop: '20px' }}>
-                    Genesis Pools
-                  </Typography>
-                  <Grid container spacing={3}>
-                    {activeBanks
-                      .filter((bank) => bank.sectionInUI === 0)
-                      .map((bank) => (
-                        <React.Fragment key={bank.name}>
-                          <CemeteryCard bank={bank} />
-                        </React.Fragment>
-                      ))}
-                  </Grid>
-                      </div> */}
               </Box>
             </Container>
           ) : (
--- src/views/Cemetery/CemeteryCard.js
+++ src/views/Cemetery/CemeteryCard.js
@@ -6,166 +6,209 @@ import Card from '../../components/Card';
 import TokenSymbol from '../../components/TokenSymbol';
 import useLpStats from '../../hooks/useLpStats';
 import { makeStyles } from '@material-ui/core/styles';
-import useCashStat from "../../hooks/useCashPriceInEstimatedTWAP.ts"
+import useCashStat from '../../hooks/useCashPriceInEstimatedTWAP.ts';
 
 const useStyles = makeStyles((theme) => ({
+  third: {
+    width: '50% !important',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  fullheight: {
+    height: '100% !important',
+  },
+  '@media only screen and (max-width: 600px)': {
     third: {
-      width: "50% !important",
-      marginLeft: "auto",
-      marginRight: "auto"
+      width: '100% !important',
     },
-    fullheight: {
-        height: "100% !important"
-    },
-    "@media only screen and (max-width: 600px)": {
-        third: {
-            width: "100% !important"
-        }
-    }
-  }));
+  },
+}));
 
 const CemeteryCard = () => {
   const tombFtmLpStats = useLpStats('SNO-JOE-LP');
   const tShareFtmLpStats = useLpStats('SNOSHARE-JOE-LP');
-  const snoSnoShareLpStats = useLpStats('SNO-SNOSHARE-LP')
+  const snoSnoShareLpStats = useLpStats('SNO-SNOSHARE-LP');
   const tombLPStats = useMemo(() => (tombFtmLpStats ? tombFtmLpStats : null), [tombFtmLpStats]);
   const tshareLPStats = useMemo(() => (tShareFtmLpStats ? tShareFtmLpStats : null), [tShareFtmLpStats]);
-  const snoSnoShareLPStats = useMemo(() => (snoSnoShareLpStats ? snoSnoShareLpStats : null), [snoSnoShareLpStats])
+  const snoSnoShareLPStats = useMemo(() => (snoSnoShareLpStats ? snoSnoShareLpStats : null), [snoSnoShareLpStats]);
   const classes = useStyles();
-  const snoPrice = useCashStat()
+  const snoPrice = useCashStat();
   return (
     <Grid container spacing={3}>
-    <Grid item xs={12} sm={6}>
-    <Card>
-      <CardContent align="center">
-          <Typography variant="h5" component="h2">
+      <Grid item xs={12} sm={6}>
+        <Card>
+          <CardContent align="center">
+            <Typography variant="h5" component="h2">
               SNO-JOE-LP
             </Typography>
-        <Box mt={1}>
-            <TokenSymbol symbol="SNO-JOE-LP" />
-        </Box>
-        <Box mt={2}>
-        </Box>
-        <Box mt={2}>
-          <span style={{ fontSize: '23px' }}>
-            {tombLPStats?.tokenAmount ? tombLPStats?.tokenAmount : '-.--'} SNO /{' '}
-            {tombLPStats?.ftmAmount ? tombLPStats?.ftmAmount : '-.--'} JOE
-          </span>
-        </Box>
-        <Box>${tombLPStats?.priceOfOne ? tombLPStats.priceOfOne : '-.--'}</Box>
-        <span style={{ fontSize: '12px' }}>
-          Liquidity: ${tombLPStats?.totalLiquidity ? tombLPStats.totalLiquidity : '-.--'} <br />
-          Total supply: {tombLPStats?.totalSupply ? tombLPStats.totalSupply : '-.--'}
-        </span>
-      </CardContent>
-      <CardActions style={{ justifyContent: 'center' }}>
-          <Button color="primary" style={{ width: '150px', height: "45px", marginBottom: '5%' }} variant="contained" component={Link} to={`/farms/SnoJoeLPSnoShareRewardPool/`}>
-            Farm
-          </Button>
-          <Button color="primary" target="_blank" style={{ width: '150px', height: "45px", marginBottom: '5%' }} variant="contained" href="https://traderjoexyz.com/pool/0x1fE4869f2C5181b9CD780a7E16194FA2c4C4293D/0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd">
+            <Box mt={1}>
+              <TokenSymbol symbol="SNO-JOE-LP" />
+            </Box>
+            <Box mt={2}></Box>
+            <Box mt={2}>
+              <span style={{ fontSize: '23px' }}>
+                {tombLPStats?.tokenAmount ? tombLPStats?.tokenAmount : '-.--'} SNO /{' '}
+                {tombLPStats?.ftmAmount ? tombLPStats?.ftmAmount : '-.--'} JOE
+              </span>
+            </Box>
+            <Box>${tombLPStats?.priceOfOne ? tombLPStats.priceOfOne : '-.--'}</Box>
+            <span style={{ fontSize: '12px' }}>
+              Liquidity: ${tombLPStats?.totalLiquidity ? tombLPStats.totalLiquidity : '-.--'} <br />
+              Total supply: {tombLPStats?.totalSupply ? tombLPStats.totalSupply : '-.--'}
+            </span>
+          </CardContent>
+          <CardActions style={{ justifyContent: 'center' }}>
+            <Button
+              color="primary"
+              style={{ width: '150px', height: '45px', marginBottom: '5%' }}
+              variant="contained"
+              component={Link}
+              to={`/farms/SnoJoeLPSnoShareRewardPool/`}
+            >
+              Farm
+            </Button>
+            <Button
+              color="primary"
+              target="_blank"
+              style={{ width: '150px', height: '45px', marginBottom: '5%' }}
+              variant="contained"
+              href="https://traderjoexyz.com/pool/0x1fE4869f2C5181b9CD780a7E16194FA2c4C4293D/0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd"
+            >
               Add Liquidity
             </Button>
-        </CardActions>
-    </Card>
-    </Grid>
-    <Grid item xs={12} sm={6}>
-      <Card>
-        <CardContent align="center">
+          </CardActions>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Card>
+          <CardContent align="center">
             <Typography variant="h5" component="h2">
-            SNOSHARE-JOE-LP
-              </Typography>
-          <Box mt={1}>
+              SNOSHARE-JOE-LP
+            </Typography>
+            <Box mt={1}>
               <TokenSymbol symbol="SNOSHARE-JOE-LP" />
-          </Box>
-          <Box mt={2}>
-          </Box>
-          <Box mt={2}>
-          <span style={{ fontSize: '23px' }}>
-                  {tshareLPStats?.tokenAmount ? tshareLPStats?.tokenAmount : '-.--'} SNOSHARE /{' '}
-                  {tshareLPStats?.ftmAmount ? tshareLPStats?.ftmAmount : '-.--'} JOE
-                </span>
-          </Box>
-          <Box>${tshareLPStats?.priceOfOne ? tshareLPStats.priceOfOne : '-.--'}</Box>
-              <span style={{ fontSize: '12px' }}>
-                Liquidity: ${tshareLPStats?.totalLiquidity ? tshareLPStats.totalLiquidity : '-.--'}
-                <br />
-                Total supply: {tshareLPStats?.totalSupply ? tshareLPStats.totalSupply : '-.--'}
+            </Box>
+            <Box mt={2}></Box>
+            <Box mt={2}>
+              <span style={{ fontSize: '23px' }}>
+                {tshareLPStats?.tokenAmount ? tshareLPStats?.tokenAmount : '-.--'} SNOSHARE /{' '}
+                {tshareLPStats?.ftmAmount ? tshareLPStats?.ftmAmount : '-.--'} JOE
               </span>
-        </CardContent>
-        <CardActions style={{ justifyContent: 'center' }}>
-            <Button color="primary" style={{ width: '150px', height: "45px", marginBottom: '5%' }} variant="contained" component={Link} to={`/farms/SnoShareJoeLPSnoShareRewardPool/`}>
+            </Box>
+            <Box>${tshareLPStats?.priceOfOne ? tshareLPStats.priceOfOne : '-.--'}</Box>
+            <span style={{ fontSize: '12px' }}>
+              Liquidity: ${tshareLPStats?.totalLiquidity ? tshareLPStats.totalLiquidity : '-.--'}
+              <br />
+              Total supply: {tshareLPStats?.totalSupply ? tshareLPStats.totalSupply : '-.--'}
+            </span>
+          </CardContent>
+          <CardActions style={{ justifyContent: 'center' }}>
+            <Button
+              color="primary"
+              style={{ width: '150px', height: '45px', marginBottom: '5%' }}
+              variant="contained"
+              component={Link}
+              to={`/farms/SnoShareJoeLPSnoShareRewardPool/`}
+            >
               Farm
             </Button>
-            <Button color="primary" target="_blank" style={{ width: '150px', height: "45px", marginBottom: '5%' }} variant="contained" href="https://traderjoexyz.com/pool/0xe7A102Fbc8AB3581d62830DdB599eCCaae5e7875/0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd" >
+            <Button
+              color="primary"
+              target="_blank"
+              style={{ width: '150px', height: '45px', marginBottom: '5%' }}
+              variant="contained"
+              href="https://traderjoexyz.com/pool/0xe7A102Fbc8AB3581d62830DdB599eCCaae5e7875/0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd"
+            >
               Add Liquidity
             </Button>
           </CardActions>
-      </Card>
+        </Card>
       </Grid>
       <Grid item xs={12} sm={6}>
-      <Card>
-        <CardContent align="center">
+        <Card>
+          <CardContent align="center">
             <Typography variant="h5" component="h2">
-            SNO-SNOSHARE-LP
-              </Typography>
-          <Box mt={1}>
+              SNO-SNOSHARE-LP
+            </Typography>
+            <Box mt={1}>
               <TokenSymbol symbol="SNO-SNOSHARE-LP" />
-          </Box>
-          <Box mt={2}>
-          </Box>
-          <Box mt={2}>
-          <span style={{ fontSize: '23px' }}>
-                  {snoSnoShareLPStats?.ftmAmount ? snoSnoShareLPStats?.ftmAmount : '-.--'} SNO /{' '}
-                  {snoSnoShareLPStats?.tokenAmount ? snoSnoShareLPStats?.tokenAmount : '-.--'} SNOSHARE
-                </span>
-          </Box>
-          <Box>${snoSnoShareLPStats?.priceOfOne ? snoSnoShareLPStats.priceOfOne : '-.--'}</Box>
-              <span style={{ fontSize: '12px' }}>
-                Liquidity: ${snoSnoShareLPStats?.totalLiquidity ? snoSnoShareLPStats.totalLiquidity : '-.--'}
-                <br />
-                Total supply: {snoSnoShareLPStats?.totalSupply ? snoSnoShareLPStats.totalSupply : '-.--'}
+            </Box>
+            <Box mt={2}></Box>
+            <Box mt={2}>
+              <span style={{ fontSize: '23px' }}>
+                {snoSnoShareLPStats?.ftmAmount ? snoSnoShareLPStats?.ftmAmount : '-.--'} SNO /{' '}
+                {snoSnoShareLPStats?.tokenAmount ? snoSnoShareLPStats?.tokenAmount : '-.--'} SNOSHARE
               </span>
-        </CardContent>
-        <CardActions style={{ justifyContent: 'center' }}>
-            <Button color="primary" style={{ width: '150px', height: "45px", marginBottom: '5%' }} variant="contained" component={Link} to={`/farms/SnoSnoShareLPSnoShareRewardPool/`}>
+            </Box>
+            <Box>${snoSnoShareLPStats?.priceOfOne ? snoSnoShareLPStats.priceOfOne : '-.--'}</Box>
+            <span style={{ fontSize: '12px' }}>
+              Liquidity: ${snoSnoShareLPStats?.totalLiquidity ? snoSnoShareLPStats.totalLiquidity : '-.--'}
+              <br />
+              Total supply: {snoSnoShareLPStats?.totalSupply ? snoSnoShareLPStats.totalSupply : '-.--'}
+            </span>
+          </CardContent>
+          <CardActions style={{ justifyContent: 'center' }}>
+            <Button
+              color="primary"
+              style={{ width: '150px', height: '45px', marginBottom: '5%' }}
+              variant="contained"
+              component={Link}
+              to={`/farms/SnoSnoShareLPSnoShareRewardPool/`}
+            >
               Farm
             </Button>
-            <Button color="primary" target="_blank" style={{ width: '150px', height: "45px", marginBottom: '5%' }} variant="contained" href="https://traderjoexyz.com/pool/0x1fE4869f2C5181b9CD780a7E16194FA2c4C4293D/0xe7A102Fbc8AB3581d62830DdB599eCCaae5e7875" >
+            <Button
+              color="primary"
+              target="_blank"
+              style={{ width: '150px', height: '45px', marginBottom: '5%' }}
+              variant="contained"
+              href="https://traderjoexyz.com/pool/0x1fE4869f2C5181b9CD780a7E16194FA2c4C4293D/0xe7A102Fbc8AB3581d62830DdB599eCCaae5e7875"
+            >
               Add Liquidity
             </Button>
           </CardActions>
-      </Card>
+        </Card>
       </Grid>
       <Grid item xs={12} sm={6}>
-      <Card>
-        <CardContent align="center">
+        <Card>
+          <CardContent align="center">
             <Typography variant="h5" component="h2">
-            SNO
-              </Typography>
-          <Box mt={1}>
+              SNO
+            </Typography>
+            <Box mt={1}>
               <TokenSymbol symbol="SNO" />
-          </Box>
-          <Box mt={2}>
-              SINGLE STAKE
-          </Box>
-          <Box>${snoPrice?.priceInDollars ? snoPrice?.priceInDollars : '-.--'}</Box>
-              <span style={{ fontSize: '12px' }}>
-                Circulating Supply: ${snoPrice?.circulatingSupply ? snoPrice?.circulatingSupply : '-.--'}
-                <br />
-                Total supply: {snoPrice?.totalSupply ? snoPrice.totalSupply : '-.--'}
-              </span>
-        </CardContent>
-        <CardActions style={{ justifyContent: 'center' }}>
-            <Button color="primary" style={{ width: '150px', height: "45px", marginBottom: '5%' }} variant="contained" component={Link} to={`/farms/SnoSnoShareRewardPool/`}>
+            </Box>
+            <Box mt={2}>SINGLE STAKE</Box>
+            <Box>${snoPrice?.priceInDollars ? snoPrice?.priceInDollars : '-.--'}</Box>
+            <span style={{ fontSize: '12px' }}>
+              Circulating Supply: ${snoPrice?.circulatingSupply ? snoPrice?.circulatingSupply : '-.--'}
+              <br />
+              Total supply: {snoPrice?.totalSupply ? snoPrice.totalSupply : '-.--'}
+            </span>
+          </CardContent>
+          <CardActions style={{ justifyContent: 'center' }}>
+            <Button
+              color="primary"
+              style={{ width: '150px', height: '45px', marginBottom: '5%' }}
+              variant="contained"
+              component={Link}
+              to={`/farms/SnoSnoShareRewardPool/`}
+            >
               Farm
             </Button>
-            <Button color="primary" target="_blank" style={{ width: '150px', height: "45px", marginBottom: '5%' }} variant="contained" href="https://traderjoexyz.com/trade?inputCurrency=0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd&outputCurrency=0x1fE4869f2C5181b9CD780a7E16194FA2c4C4293D" >
+            <Button
+              color="primary"
+              target="_blank"
+              style={{ width: '150px', height: '45px', marginBottom: '5%' }}
+              variant="contained"
+              href="https://traderjoexyz.com/trade?inputCurrency=0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd&outputCurrency=0x1fE4869f2C5181b9CD780a7E16194FA2c4C4293D"
+            >
               Buy SNO
             </Button>
           </CardActions>
-      </Card>
+        </Card>
       </Grid>
-      </Grid>
+    </Grid>
   );
 };
 
